1fbfd362b69a0cdab79ea38b1ceb2da941aaa93c
Vayda, Oleksandr: IT (PRG) <Oleksandr.Vayda@barclayscapital.com>
2017-08-02 20:25:37 +0200

    SL-36 Check serialized lineage version before attempting to deserialize it. Rename "hash" to "_hash"

--- persistence/mongo/src/main/scala/za/co/absa/spline/persistence/mongo/MongoDataLineagePersistor.scala
+++ persistence/mongo/src/main/scala/za/co/absa/spline/persistence/mongo/MongoDataLineagePersistor.scala
@@ -19,11 +19,10 @@ package za.co.absa.spline.persistence.mongo
 import java.util.UUID
 
 import _root_.salat._
-import za.co.absa.spline.persistence.api.DataLineageHashResolver
-import za.co.absa.spline.model.{DataLineage, DataLineageDescriptor}
-import za.co.absa.spline.persistence.api.DataLineagePersistor
 import com.mongodb.casbah.Imports._
 import com.mongodb.casbah.MongoClient
+import za.co.absa.spline.model.{DataLineage, DataLineageDescriptor}
+import za.co.absa.spline.persistence.api.{DataLineageHashResolver, DataLineagePersistor}
 
 import scala.collection.JavaConverters._
 
@@ -32,6 +31,7 @@ import scala.collection.JavaConverters._
   */
 class MongoDataLineagePersistor(dbUrl: String, dbName: String) extends DataLineagePersistor {
   val dataLineageCollectionName: String = "DataSets"
+  val LATEST_SERIAL_VERSION = 1
 
   private val client: MongoClient = MongoClient(MongoClientURI(dbUrl))
   private val database = client.getDB(dbName)
@@ -42,25 +42,32 @@ class MongoDataLineagePersistor(dbUrl: String, dbName: String) extends DataLinea
   override def store(lineage: DataLineage): Unit = {
     val dbo = grater[DataLineage].asDBObject(lineage)
     val hash = DataLineageHashResolver.resolve(lineage)
-    dbo.put("hash", hash)
+    dbo.put("_hash", hash)
+    dbo.put("_ver", LATEST_SERIAL_VERSION)
     dataLineageCollection.insert(dbo)
   }
 
   override def load(id: UUID): Option[DataLineage] =
-    Option(dataLineageCollection findOne id) map (grater[DataLineage].asObject(_))
+    Option(dataLineageCollection findOne id) map withVersionCheck(grater[DataLineage].asObject(_))
 
   override def remove(id: UUID): Unit = dataLineageCollection remove DBObject("_id" -> id)
 
   override def exists(lineage: DataLineage): Option[UUID] = {
     val hash = DataLineageHashResolver.resolve(lineage)
-    val key = DBObject("appName" -> lineage.appName, "hash" -> hash)
+    val key = DBObject("appName" -> lineage.appName, "_hash" -> hash)
     Option(dataLineageCollection.findOne(key, DBObject("_id" -> 1))) map (_.get("_id").asInstanceOf[UUID])
   }
 
   override def list(): Iterator[DataLineageDescriptor] =
     dataLineageCollection
-      .find(MongoDBObject(), MongoDBObject("_id" -> 1, "appName" -> 1))
+      .find(MongoDBObject(), MongoDBObject("_id" -> 1, "_ver" -> 1, "appName" -> 1))
       .iterator.asScala
-      .map(grater[DataLineageDescriptor].asObject(_))
+      .map(withVersionCheck(grater[DataLineageDescriptor].asObject(_)))
+
+  private def withVersionCheck[T](f: DBObject => T): DBObject => T =
+    dbo => (dbo get "_ver").asInstanceOf[Int] match {
+      case LATEST_SERIAL_VERSION => f(dbo)
+      case unknownVersion => sys.error(s"Unsupported serialized lineage version: $unknownVersion")
+    }
 
 }
