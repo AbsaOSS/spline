f8ded352ab0ee120ab501061c406b1e0095b5556
Marek Novotny <marek.x.novotny@barclays.com>
2017-10-18 12:24:57 +0200

    SL-69 - The first version of sample jobs for high-level lineage

---
+++ sample/src/main/scala/za/co/absa/spline/sample/HighLevelSampleJob1.scala
@@ -0,0 +1,37 @@
+package za.co.absa.spline.sample
+
+import org.apache.spark.sql.SparkSession
+
+object HighLevelSampleJob1 {
+  def main(args: Array[String]) {
+    val spark = SparkSession.builder()
+      .appName("High-level Lineage Job 1")
+      .master("local[*]")
+      .getOrCreate()
+
+
+    import spark.implicits._
+    import za.co.absa.spline.core.SparkLineageInitializer._
+    spark.enableLineageTracking()
+
+    val input = spark.read.option("header", "true").csv("data/input/highLevelLineageData.csv")
+
+    val cleaned = input.select(
+      $"Country Name" as "country",
+      $"Series Name" as "metric",
+      $"2012 [YR2012]" as "2012",
+      $"2013 [YR2013]" as "2013",
+      $"2014 [YR2014]" as "2014",
+      $"2015 [YR2015]" as "2015",
+      $"2016 [YR2016]" as "2016"
+    ).cache
+
+    val renewableEnergyPercent = cleaned.filter($"metric" === "Renewable electricity output (% of total electricity output)")
+
+    renewableEnergyPercent.write.mode("overwrite").parquet("data/results/renewableEnergyPercent")
+
+    val gdpPerCapitalUSD = cleaned.filter($"metric" === "GDP per capita (current US$)")
+
+    gdpPerCapitalUSD.write.mode("overwrite").parquet("data/results/gdpPerCapitalUSD")
+  }
+}
---
+++ sample/src/main/scala/za/co/absa/spline/sample/HighLevelSampleJob2.scala
@@ -0,0 +1,30 @@
+package za.co.absa.spline.sample
+
+import org.apache.spark.sql.SparkSession
+
+object HighLevelSampleJob2 {
+  def main(args: Array[String]) {
+    val spark = SparkSession.builder()
+      .appName("High-level Lineage Job 2")
+      .config("spark.sql.shuffle.partitions", "4")
+      .master("local[*]")
+      .getOrCreate()
+
+    import spark.implicits._
+    import za.co.absa.spline.core.SparkLineageInitializer._
+    spark.enableLineageTracking()
+
+    val renewable = spark.read.parquet("data/results/renewableEnergyPercent")
+      .select($"*", ($"2013" - $"2012") as "1yrGrowthEnergy")
+
+    val gdp = spark.read.parquet("data/results/gdpPerCapitalUSD")
+      .select($"*", ($"2013" - $"2012") as "1yrGrowthGDP")
+
+    val overall = renewable.as("renew")
+      .join(gdp.as("gdp"), $"renew.country" === $"gdp.country")
+      .select($"renew.country", $"1yrGrowthEnergy", $"1yrGrowthGDP")
+      .orderBy($"1yrGrowthEnergy" desc)
+
+    overall.write.mode("overwrite").parquet("greenestCountries")
+  }
+}
