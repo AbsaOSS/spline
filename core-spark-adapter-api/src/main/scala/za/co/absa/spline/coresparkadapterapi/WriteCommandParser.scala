c505850266221a8b1cf7785380648605230ee5f4
Vayda, Oleksandr: IT (PRG) <Oleksandr.Vayda@barclayscapital.com>
2018-03-27 15:04:34 +0200

    SL-116 Add missing Apache licence headers. Get rid of unnecessary type casting implicit.

--- core-spark-adapter-api/src/main/scala/za/co/absa/spline/coresparkadapterapi/WriteCommandParser.scala
+++ core-spark-adapter-api/src/main/scala/za/co/absa/spline/coresparkadapterapi/WriteCommandParser.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Barclays Africa Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package za.co.absa.spline.coresparkadapterapi
 
 import org.apache.spark.sql.SaveMode
@@ -7,16 +23,11 @@ import scala.language.implicitConversions
 import scala.reflect.ClassTag
 
 abstract class WriteCommandParser[T <: LogicalPlan](implicit tag: ClassTag[T]) {
-
-  implicit protected def asSparkSpecific(operation: LogicalPlan): T = {
-    operation.asInstanceOf[T]
-  }
-
   def matches(operation: LogicalPlan): Boolean = {
     tag.runtimeClass.isAssignableFrom(operation.getClass)
   }
 
-  implicit def asWriteCommand(operation: LogicalPlan): WriteCommand
+  implicit def asWriteCommand(operation: T): WriteCommand
 
 }
 
