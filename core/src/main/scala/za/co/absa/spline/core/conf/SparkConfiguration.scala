1bd83a4da458d133f25466b07fa2c6e725a08626
Vayda, Oleksandr: IT (PRG) <Oleksandr.Vayda@barclayscapital.com>
2018-07-09 12:36:57 +0200

    SL-140 SparkConf properties should start with "spark." prefix

--- core/src/main/scala/za/co/absa/spline/core/conf/SparkConfiguration.scala
+++ core/src/main/scala/za/co/absa/spline/core/conf/SparkConfiguration.scala
@@ -30,11 +30,22 @@ import scala.collection.JavaConverters._
  */
 class SparkConfiguration(conf: SparkConf) extends ReadOnlyConfiguration {
 
-  override def getProperty(key: String): AnyRef = conf get key
+  import SparkConfiguration._
 
-  override def getKeys: util.Iterator[String] = conf.getAll.iterator.map(_._1).asJava
+  override def getProperty(key: String): AnyRef =
+    try
+      conf get s"$KEY_PREFIX$key"
+    catch {
+      case _: NoSuchElementException => null
+    }
 
-  override def containsKey(key: String): Boolean = conf contains key
+  override def getKeys: util.Iterator[String] = conf.getAll.iterator.map(_._1.drop(KEY_PREFIX.length)).asJava
+
+  override def containsKey(key: String): Boolean = conf contains s"$KEY_PREFIX$key"
 
   override def isEmpty: Boolean = conf.getAll.isEmpty
+}
+
+object SparkConfiguration {
+  private val KEY_PREFIX = "spark."
 }
