881d85f53e00291bb5bee9d788ac32b0cae6e862
Vayda, Oleksandr: IT (PRG) <Oleksandr.Vayda@barclayscapital.com>
2018-10-08 10:53:42 +0200

    SL-145 Add "X-SPLINE-TIMEOUT" HTTP header to fine control async http requests' timeouts.

--- web/src/main/scala/za/co/absa/spline/web/handler/ScalaFutureMethodReturnValueHandler.scala
+++ web/src/main/scala/za/co/absa/spline/web/handler/ScalaFutureMethodReturnValueHandler.scala
@@ -16,6 +16,7 @@
 
 package za.co.absa.spline.web.handler
 
+import org.apache.commons.lang.math.NumberUtils
 import org.springframework.core.MethodParameter
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.context.request.async.{DeferredResult, WebAsyncUtils}
@@ -26,24 +27,33 @@ import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
 class ScalaFutureMethodReturnValueHandler(implicit ec: ExecutionContext) extends DeferredResultMethodReturnValueHandler with AsyncHandlerMethodReturnValueHandler {
+
+  import ScalaFutureMethodReturnValueHandler._
+
   override def supportsReturnType(returnType: MethodParameter): Boolean = isFutureClass(returnType.getParameterType) || super.supportsReturnType(returnType)
 
   override def isAsyncReturnValue(returnValue: scala.Any, returnType: MethodParameter): Boolean = isFutureClass(returnType.getParameterType)
 
-  private def isFutureClass(clazz: Class[_]): Boolean = classOf[Future[_]].isAssignableFrom(clazz)
-
   override def handleReturnValue(retVal: scala.Any, retType: MethodParameter, mavContainer: ModelAndViewContainer, req: NativeWebRequest): Unit =
     retVal match {
       case future: Future[_] =>
-        val deferredResult = adaptToDeferredResult(future)
+        val timeout = NumberUtils.createLong(req.getHeader(TIMEOUT_HEADER))
+        val deferredResult = toDeferredResult(future, timeout)
         WebAsyncUtils.getAsyncManager(req).startDeferredResultProcessing(deferredResult, mavContainer)
       case _ => super.handleReturnValue(retVal, retType, mavContainer, req)
     }
 
-  private def adaptToDeferredResult[T](returnValue: Future[T]): DeferredResult[T] = new DeferredResult[T] {
-    returnValue.asInstanceOf[Future[T]] andThen {
-      case Success(value) => setResult(value)
-      case Failure(error) => setErrorResult(error)
+  private def toDeferredResult[T](returnValue: Future[T], timeout: java.lang.Long): DeferredResult[T] =
+    new DeferredResult[T](timeout) {
+      returnValue.asInstanceOf[Future[T]] andThen {
+        case Success(value) => setResult(value)
+        case Failure(error) => setErrorResult(error)
+      }
     }
-  }
+}
+
+object ScalaFutureMethodReturnValueHandler {
+  private val TIMEOUT_HEADER = "X-SPLINE-TIMEOUT"
+
+  private def isFutureClass(clazz: Class[_]): Boolean = classOf[Future[_]].isAssignableFrom(clazz)
 }
