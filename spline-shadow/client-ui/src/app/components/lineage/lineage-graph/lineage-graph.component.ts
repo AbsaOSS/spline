4bb32a7d4b360d54e12777cda8c15c1123d126cf
Alex Vayda <oleksandr.vayda@gmail.com>
2019-01-23 13:28:03 +0100

    spline #91 - update licence headers

--- spline-shadow/client-ui/src/app/components/lineage/lineage-graph/lineage-graph.component.ts
+++ spline-shadow/client-ui/src/app/components/lineage/lineage-graph/lineage-graph.component.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { Component, ViewChild, OnInit, Output, EventEmitter } from '@angular/core';
 import { CytoscapeNgLibComponent } from 'cytoscape-ng-lib';
 import { GraphService } from 'src/app/services/lineage/graph.service';
