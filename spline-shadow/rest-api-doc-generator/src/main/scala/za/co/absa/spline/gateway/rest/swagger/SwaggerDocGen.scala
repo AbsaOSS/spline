648e1ff8fdf473b9c0157d029518cc624a082685
wajda <oleksandr.vayda@gmail.com>
2019-01-31 00:22:56 +0100

    spline-87 Create Swagger doc generator tool

---
+++ spline-shadow/rest-api-doc-generator/src/main/scala/za/co/absa/spline/gateway/rest/swagger/SwaggerDocGen.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.spline.gateway.rest.swagger
+
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.mock.web.MockServletContext
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders._
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers._
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext
+import za.co.absa.spline.gateway.rest.RESTConfig
+
+object SwaggerDocGen {
+  def generate: String =
+    MockMvcBuilders
+      .webAppContextSetup(new AnnotationConfigWebApplicationContext {
+        register(classOf[RESTConfig])
+        setServletContext(new MockServletContext())
+        refresh()
+      })
+      .build
+      .perform(get("/v2/api-docs") accept APPLICATION_JSON)
+      .andExpect(status.isOk)
+      .andReturn
+      .getResponse
+      .getContentAsString
+}
