15172f089a9ef82f4af0e832d40358386d1335d9
lukas.nalezenec <lukas.nalezenec@gmail.com>
2019-05-02 15:31:45 +0200

    spline-217 Spline test fixure

--- integration-tests/src/test/scala/za/co/absa/spline/BasicIntegrationTests.scala
+++ integration-tests/src/test/scala/za/co/absa/spline/BasicIntegrationTests.scala
@@ -21,86 +21,106 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
 import org.apache.spark.sql.{Row, SaveMode}
 import org.scalatest._
+import org.slf4s.Logging
+import za.co.absa.spline.DataFrameImplicits._
 import za.co.absa.spline.common.TempDirectory
-import za.co.absa.spline.fixture.{AbstractSplineFixture, AsyncSparkFixture, AsyncSplineFixture}
+import za.co.absa.spline.fixture._
+import za.co.absa.spline.fixture.spline.SplineFixture
 import za.co.absa.spline.model.DataLineage
-import za.co.absa.spline.model.op.{Write}
+import za.co.absa.spline.model.op.Write
 
-/** Contains smoke tests for basic operations.*/
-//Ignored because we cannot have two AsyncSplineFixture based tests in
-// one project. This will work in release 4
-@Ignore class BasicIntegrationTests
-  extends AsyncFlatSpec
-    with Matchers
-    with AsyncSparkFixture
-    with AsyncSplineFixture {
+/** Contains smoke tests for basic operations. */
+class BasicIntegrationTests extends FlatSpec
+  with Matchers
+  with SparkFixture
+  with SplineFixture
+  with Logging {
 
-  import spark.implicits._
+  "saveAsTable" should "process all operations" in
+    withSparkSession(spark =>
+      withLineageTracking(spark) {
+        lineageCaptor => {
+          import spark.implicits._
 
-  "saveAsTable" should "process all operations" in {
+          val df = Seq((1, 2), (3, 4)).toDF().agg(concat(sum('_1), min('_2)) as "forty_two")
 
-    val df = Seq((1, 2), (3, 4)).toDF().agg(concat(sum('_1), min('_2)) as "forty_two")
-    val saveAsTable: DataLineage = df.saveAsTableLineage("someTable")
+          val lineage = lineageCaptor.lineageOf(df.writeToTable("someTable"))
 
-    spark.sql("drop table someTable")
-    saveAsTable.operations.length shouldBe 3
-  }
+          spark.sql("drop table someTable")
+          lineage.operations.length shouldBe 3
+        }
+      })
 
-  "save_to_fs" should "process all operations" in {
+  "save_to_fs" should "process all operations" in
+    withSparkSession(spark =>
+      withLineageTracking(spark) {
+        lineageCaptor => {
+          import spark.implicits._
 
-    val df = Seq((1, 2), (3, 4)).toDF().agg(concat(sum('_1), min('_2)) as "forty_two")
-    val saveToFS: DataLineage = df.writtenLineage()
+          val df = Seq((1, 2), (3, 4)).toDF().agg(concat(sum('_1), min('_2)) as "forty_two")
+          val lineage = lineageCaptor.lineageOf(df.writeToDisk())
 
-    saveToFS.operations.length shouldBe 3
-  }
+          lineage.operations.length shouldBe 3
+        }
+      })
 
-  "saveAsTable" should "use URIS compatible with filesystem write" in {
+  "saveAsTable" should "use URIS compatible with filesystem write" in
+    withSparkSession(spark =>
+      withLineageTracking(spark) {
+        lineageCaptor => {
 
-    //When I write something to table and then read it again, Spline have to use matching URI.
+          //When I write something to table and then read it again, Spline have to use matching URI.
 
-    val tableName = "externalTable"
-    val dir = TempDirectory ("sparkunit", "table", false).deleteOnExit()
-    val path = dir.path.toString.replace("\\", "/")
-    val sql = "create table " + tableName + " (num int) using parquet location '" + path + "' "
-    spark.sql(sql)
+          val tableName = "externalTable"
+          val dir = TempDirectory("sparkunit", "table").deleteOnExit()
+          val path = dir.path.toString.replace("\\", "/")
+          val sql = "create table " + tableName + " (num int) using parquet location '" + path + "' "
+          spark.sql(sql)
 
-    val schema: StructType = StructType(List(StructField("num", IntegerType, true)))
-    val data = spark.sparkContext.parallelize(Seq(Row(1), Row(3)))
-    val inputDf = spark.sqlContext.createDataFrame(data, schema)
+          val schema: StructType = StructType(List(StructField("num", IntegerType, nullable = true)))
+          val data = spark.sparkContext.parallelize(Seq(Row(1), Row(3)))
+          val inputDf = spark.sqlContext.createDataFrame(data, schema)
 
-    val writeToTable: DataLineage = inputDf.saveAsTableLineage(tableName, SaveMode.Append)
+          val lineage: DataLineage = lineageCaptor.lineageOf {
+            inputDf.writeToTable(tableName, SaveMode.Append)
+          }
 
-    val write1: Write = writeToTable.operations.filter(_.isInstanceOf[Write]).head.asInstanceOf[Write]
-    val saveAsTablePath = write1.path
+          val write1: Write = lineage.operations.filter(_.isInstanceOf[Write]).head.asInstanceOf[Write]
+          val saveAsTablePath = write1.path
 
-    AbstractSplineFixture.resetCapturedLineage
-    spark.sql("drop table " + tableName)
+          val readFromTable: DataLineage = lineageCaptor.lineageOf {
+            spark.sql("drop table " + tableName)
+            inputDf.writeToDisk(path, SaveMode.Overwrite)
+          }
 
-    val readFromTable: DataLineage = inputDf.writtenLineage(path, SaveMode.Overwrite)
+          val writeOperation = readFromTable.operations.filter(_.isInstanceOf[Write]).head.asInstanceOf[Write]
+          val write2 = writeOperation.path
 
-    val writeOperation = readFromTable.operations.filter(_.isInstanceOf[Write]).head.asInstanceOf[Write]
-    val write2 = writeOperation.path
+          saveAsTablePath shouldBe write2
+        }
+      })
 
-    saveAsTablePath shouldBe write2
-  }
+  "saveAsTable" should "use table path as identifier when writing to external table" in
+    withSparkSession(spark =>
+      withLineageTracking(spark) {
+        lineageCaptor => {
+          val dir = TempDirectory("sparkunit", "table", pathOnly = true).deleteOnExit()
+          val expectedPath = dir.path.toUri.toURL
+          val path = dir.path.toString.replace("\\", "/")
+          val sql = "create table e_table(num int) using parquet location '" + path + "' "
+          spark.sql(sql)
 
-  "saveAsTable" should "use table path as identifier when writing to external table" in {
-    val dir = TempDirectory ("sparkunit", "table", true).deleteOnExit()
-    val expectedPath = dir.path.toUri.toURL
-    val path = dir.path.toString.replace("\\", "/")
-    val sql = "create table e_table(num int) using parquet location '" + path + "' "
-    spark.sql(sql)
+          val schema: StructType = StructType(List(StructField("num", IntegerType, nullable = true)))
+          val data = spark.sparkContext.parallelize(Seq(Row(1), Row(3)))
+          val df = spark.sqlContext.createDataFrame(data, schema)
 
-    val schema: StructType = StructType(List(StructField("num", IntegerType, true)))
-    val data = spark.sparkContext.parallelize(Seq(Row(1), Row(3)))
-    val df = spark.sqlContext.createDataFrame(data, schema)
+          val wt: DataLineage = lineageCaptor.lineageOf(df.writeToTable("e_table", SaveMode.Append))
 
-    val writeToTable: DataLineage = df.saveAsTableLineage("e_table", SaveMode.Append)
+          val writeOperation: Write = wt.operations.filter(_.isInstanceOf[Write]).head.asInstanceOf[Write]
 
-    val writeOperation: Write = writeToTable.operations.filter(_.isInstanceOf[Write]).head.asInstanceOf[Write]
-
-    new Path(writeOperation.path).toUri.toURL shouldBe expectedPath
-  }
+          new Path(writeOperation.path).toUri.toURL shouldBe expectedPath
+        }
+      })
 
 
 }
