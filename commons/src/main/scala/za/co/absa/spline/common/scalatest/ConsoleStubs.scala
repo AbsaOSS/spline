a9dfaeff6c06837dcc5392091f037f2b42e58ad3
wajda <oleksandr.vayda@gmail.com>
2019-06-21 01:28:43 +0200

    (noissue) Test commons: add useful utilities for stubbing system out/err/in and capturing system exit status.

---
+++ commons/src/main/scala/za/co/absa/spline/common/scalatest/ConsoleStubs.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.spline.common.scalatest
+
+import java.io.{ByteArrayOutputStream, PrintStream, StringReader}
+import java.nio.charset.Charset
+
+import za.co.absa.spline.common.ARM._
+import za.co.absa.spline.common.scalatest.ConsoleStubs._
+
+trait ConsoleStubs {
+
+  protected def withStdIn[A](str: String)(body: => A): A = {
+    Console.withIn(new StringReader(str))(body)
+  }
+
+  protected def captureStdOut(body: => Any, charset: String = DefaultCharset): String =
+    withPrintStreamToString(charset)(Console.withOut(_)(body))
+
+  protected def captureStdErr(body: => Any, charset: String = DefaultCharset): String =
+    withPrintStreamToString(charset)(Console.withErr(_)(body))
+
+  private def withPrintStreamToString(charset: String)(fn: PrintStream => Unit): String = {
+    val baos = new ByteArrayOutputStream
+    using(new PrintStream(baos))(fn)
+    baos.toString(charset)
+  }
+}
+
+object ConsoleStubs {
+  private val DefaultCharset: String = Charset.defaultCharset.name
+}
