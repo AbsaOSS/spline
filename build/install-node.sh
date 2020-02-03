f42b291ee72e5f60b0d768e132028b4d9ddd9ae8
Oleksandr Vayda <oleksandr.vayda@gmail.com>
2020-02-03 15:11:58 +0100

    Fighting TeamCity agent crashes: Change (again) Nodejs version to 12.14.1

--- build/install-node.sh
+++ build/install-node.sh
@@ -15,7 +15,7 @@
   limitations under the License.
 '
 
-export NODE_VERSION=13.5.0
+export NODE_VERSION=12.14.1
 
 # Install NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
