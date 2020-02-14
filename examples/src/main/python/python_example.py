744e3129af6fe36ed195b6180b2e8b138643513d
wajda <oleksandr.vayda@gmail.com>
2020-02-14 19:59:21 +0100

    spline #587 Fix links in examples

--- examples/src/main/python/python_example.py
+++ examples/src/main/python/python_example.py
@@ -14,38 +14,18 @@
 # limitations under the License.
 #
 
-
-"""
-This is simplest example of how to track a lineage with Spline.
-
-1. Make sure the Spline Producer instance is running. (See README.md)
-
-2. Go to examples directory:
-  cd examples
-
-3. Build Spline Examples:
-  mvn package
-
-4. Execute `pyspark` with a Spline spark-agent-bundle corresponding to the Spark version in use:
-  pyspark \
-    --jars target/spline-examples-0.4.0.jar \
-    --packages za.co.absa.spline:spark-agent-bundle-2.4:0.4.0 \
-    --conf spline.producer.url=http://localhost:8888/producer
-
-5. Execute the rest of this file inside PySpark.
-"""
-
-# Enable Spline tracking:
-sc._jvm.za.co.absa.spline.harvester\
+# Enable Spline tracking.
+# For Spark 2.3+ we recommend the codeless approach to enable Spline - by setting spark.sql.queryExecutionListeners
+# (See: examples/README.md)
+# Otherwise execute the following method to enable Spline manually.
+sc._jvm.za.co.absa.spline.harvester \
     .SparkLineageInitializer.enableLineageTracking(spark._jsparkSession)
 
-# Execute a job:
-spark.read\
-    .option("header", "true")\
-    .option("inferschema", "true")\
-    .csv("data/input/batch/wikidata.csv")\
-    .write\
-    .mode('overwrite')\
+# Execute a Spark job as usual:
+spark.read \
+    .option("header", "true") \
+    .option("inferschema", "true") \
+    .csv("data/input/batch/wikidata.csv") \
+    .write \
+    .mode('overwrite') \
     .csv("data/output/batch/python-sample.csv")
-
-# Review lineage on the Spline UI.
