148fd6ae29c45b22bc23fe623ab0a36eb2bdc85c
Oleksandr Korchak <oleksandr.korchak@gmail.com>
2020-04-23 14:26:55 +0200

    #648 :: Copyrights.

--- client-ui/src/app/components/lineage/lineage-highlights-toolbar/lineage-highlights-toolbar.component.less
+++ client-ui/src/app/components/lineage/lineage-highlights-toolbar/lineage-highlights-toolbar.component.less
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 ABSA Group Limited
+ * Copyright 2020 ABSA Group Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
