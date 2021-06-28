1406197cdeae93f78b4b99b241209bb25ee439f6
Oleksandr Vayda <oleksandr.vayda@gmail.com>
2021-06-28 20:43:32 +0200

    spline #834 reacting on PR comment: minor renaming

--- admin/src/test/scala/za/co/absa/spline/admin/AdminCLISpec.scala
+++ admin/src/test/scala/za/co/absa/spline/admin/AdminCLISpec.scala
@@ -96,13 +96,13 @@ class AdminCLISpec
       } should include("--help")
     }
 
-    it should "when calling with option -k, create a non-validating SSLContext" in {
+    it should "when called with option -k, create a non-validating SSLContext" in {
       cli.exec(Array("db-exec", "arangodbs://foo/bar", "-k"))
       sslCtxCaptor.getValue.nonEmpty should be(true)
       sslCtxCaptor.getValue.get should be(TLSUtils.TrustingAllSSLContext)
     }
 
-    it should "when calling without option -k, do not create any custom SSLContext (leave default one)" in {
+    it should "when called without option -k, do not create any custom SSLContext (leave default one)" in {
       cli.exec(Array("db-exec", "arangodbs://foo/bar"))
       sslCtxCaptor.getValue.isEmpty should be(true)
     }
