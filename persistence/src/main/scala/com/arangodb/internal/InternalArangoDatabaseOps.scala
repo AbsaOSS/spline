c5c4bdd5dd28fc14932688dc029aba829c1d911e
Oleksandr Vayda <oleksandr.vayda@gmail.com>
2022-03-24 17:05:02 +0100

    spline #1049 Admin CLI :: 401 not authorized when user password is empty

--- persistence/src/main/scala/com/arangodb/internal/InternalArangoDatabaseOps.scala
+++ persistence/src/main/scala/com/arangodb/internal/InternalArangoDatabaseOps.scala
@@ -34,10 +34,11 @@ class InternalArangoDatabaseOps(db: ArangoDatabaseAsync)(implicit ec: ExecutionC
   def restClient: RESTClient = {
     val asyncExecutable = db.asInstanceOf[ArangoExecuteable[ArangoExecutorAsync]]
     val ArangoExecutorAsyncDestructor(vstComm) = asyncExecutable.executor
-    val VstCommunicationDestructor(ConnectionParams(hostDescription, user, password, maybeSslContext)) = vstComm
+    val VstCommunicationDestructor(ConnectionParams(hostDescription, user, maybePassword, maybeSslContext)) = vstComm
     val scheme = if (maybeSslContext.isDefined) "https" else "http"
     val host = hostDescription.getHost
     val port = hostDescription.getPort
+    val password = maybePassword.getOrElse("")
     val maybeCredentials = Option(user).map(user => new UsernamePasswordCredentials(user, password))
 
     new RESTClientApacheHttpImpl(new URI(s"$scheme://$host:$port/_db/${db.name}"), maybeCredentials, maybeSslContext)
