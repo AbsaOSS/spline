e1286e5f661b79ad342a487975b5257898081016
Daniel Kavan <dk1844@gmail.com>
2022-08-31 11:08:26 +0200

    #1017 small code updates, no change in functionality

--- test-data-generator/src/main/scala/za/co/absa/spline/testdatagen/GenCLI.scala
+++ test-data-generator/src/main/scala/za/co/absa/spline/testdatagen/GenCLI.scala
@@ -92,7 +92,7 @@ object GenCLI {
 
     val configs: Seq[ExpandedConfig] = config.expand()
 
-    val dispatcher = createDispatcher("file", config)
+    val dispatcher = createDispatcher(config)
 
     configs.foreach(config => {
       val graphType: Graph = Graph(config)
@@ -103,16 +103,15 @@ object GenCLI {
     })
   }
 
-  private def createDispatcher(name: String, config: Config): FileDispatcher = name match {
-    case "file" =>
-      config.customOutputFileName.fold {
-        new FileDispatcher(createDefaultFileName(config))
-      } { customFileName =>
-        new FileDispatcher(customFileName)
-      }
+  private def createDispatcher(config: Config): FileDispatcher = {
+    new FileDispatcher(createDefaultFileName(config))
   }
 
   private def createDefaultFileName(config: Config): String = {
-    s"${config.graphType.name}-lineage-${config.reads}reads-${config.operations}ops-${config.attributes}attr.json.txt"
+    config.customOutputFileName match {
+      case None =>  // default name assembly
+        s"${config.graphType.name}-lineage-${config.reads}reads-${config.operations}ops-${config.attributes}attr.json.txt"
+      case Some(customName) => customName
+    }
   }
 }
