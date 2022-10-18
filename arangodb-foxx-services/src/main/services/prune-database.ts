6451f9cbd713ed18247352718d51fece9a3c5d04
Daniel Kavan <dk1844@gmail.com>
2022-10-18 15:56:10 +0200

    #684 data retention - update to reflect logic from https://gist.github.com/Aditya-Sood/ecc07c9f296dbdf03d4946c5d1b4efce - naively tested with test data (multiple lineages at different times - purge with time between - correct outcome - older purged, newer kept)

--- arangodb-foxx-services/src/main/services/prune-database.ts
+++ arangodb-foxx-services/src/main/services/prune-database.ts
@@ -14,13 +14,13 @@
  * limitations under the License.
  */
 
-import { aql, db } from '@arangodb'
+import {aql, db} from '@arangodb'
 
 export function pruneBefore(timestamp) {
     console.log(`[Spline] Prune data before: ${timestamp}`)
     const t0 = Date.now()
 
-    // remove event + edges to plans
+    // STAGE 1: Cleanup event + edges to plans
 
     const refPlanIds = db._query(aql`
         FOR p IN progress
@@ -33,74 +33,97 @@ export function pruneBefore(timestamp) {
     `).toArray()
 
     const t1 = Date.now()
-    console.log({refPlanIds: [refPlanIds.length, `${t1 - t0} ms`]})
+    console.log(`Purged ${refPlanIds.length} plans, ${t1 - t0} ms`)
 
-    // remove orphan plans + edges to sources
+    // STAGE 2: No progressOf corresponding to executionPlan (Need to scan all plans, and progressOf foreach) -> delete executionPlan and related fields
 
-    const refDataSourceKeys = db._query(aql`
-        FOR epId IN ${refPlanIds}
-            LET ep = DOCUMENT(epId)
-
-            LET isEpReferred = 0 < LENGTH(FOR po IN progressOf FILTER po._to == ep._id LIMIT 1 RETURN 1)
-
-            FILTER NOT isEpReferred
-
-            LET vertsIdsWithInEdgesIds = (
-                FOR v, e  IN 1..9999
-                    OUTBOUND ep executes, follows, writesTo, readsFrom, affects, depends
-                    COLLECT vid = v._id INTO g
-                    RETURN [vid, UNIQUE(g[*].e._id)]
-            )
-
-            LET keysByCollection = (
-                FOR id IN APPEND(vertsIdsWithInEdgesIds[*][0], vertsIdsWithInEdgesIds[*][1][**])
-                    LET parsedId = PARSE_IDENTIFIER(id)
-                    COLLECT c = parsedId.collection INTO g
-                    RETURN [c, g[*].parsedId.key]
-            )
-
-            LET executesKey = FIRST(keysByCollection[* FILTER CURRENT[0] == 'executes'][1][**])
-            LET affectsKey  = FIRST(keysByCollection[* FILTER CURRENT[0] == 'affects' ][1][**])
-            LET writesToKey = FIRST(keysByCollection[* FILTER CURRENT[0] == 'writesTo'][1][**])
-
-            LET dependsKeys    = keysByCollection[* FILTER CURRENT[0] == 'depends'   ][1][**]
-            LET readsFromKeys  = keysByCollection[* FILTER CURRENT[0] == 'readsFrom' ][1][**]
-            LET followsKeys    = keysByCollection[* FILTER CURRENT[0] == 'follows'   ][1][**]
-            LET operationKeys  = keysByCollection[* FILTER CURRENT[0] == 'operation' ][1][**]
-            LET dataSourceKeys = keysByCollection[* FILTER CURRENT[0] == 'dataSource'][1][**]
-
-            REMOVE ep IN executionPlan
-            REMOVE executesKey IN executes
-            REMOVE affectsKey IN affects
-            REMOVE writesToKey IN writesTo
-
-            LET r1 = (FOR d IN dependsKeys   REMOVE d IN depends)
-            LET r2 = (FOR r IN readsFromKeys REMOVE r IN readsFrom)
-            LET r3 = (FOR f IN followsKeys   REMOVE f IN follows)
-            LET r4 = (FOR o IN operationKeys REMOVE o IN operation)
-
-            FOR dsKey IN dataSourceKeys
-                RETURN DISTINCT dsKey
+    const execPlanIDsWithProgressEventsArray = db._query(`
+        FOR execPlan IN executionPlan
+            FOR progOf IN progressOf
+                FILTER execPlan._created < ${timestamp} && progOf._to == execPlan._id
+                COLLECT ids = execPlan._id
+                RETURN ids
     `).toArray()
 
+    const orphanExecPlanIDsOlderThanThresholdDaysArray = db._query(aql`
+        FOR execPlan IN executionPlan
+            FILTER execPlan._id NOT IN ${execPlanIDsWithProgressEventsArray} && execPlan._created < ${timestamp}
+            RETURN execPlan._id
+    `).toArray()
+
+    const collections = ['executes', 'operation', 'follows', 'uses', 'expression', 'affects', 'depends', 'writesTo', 'readsFrom', 'emits', 'produces', 'consistsOf', 'derivesFrom', 'computedBy', 'takes', 'schema', 'attribute']
+    for (let i = 0; i < collections.length; i++) {
+        console.log('### Working on', collections[i], 'collection')
+        let startCount = db._query('RETURN COUNT(@@cols)', {'@cols': collections[i]}).toArray()[0]
+        db._query('FOR orphanExecPlanID IN @arr FOR collectionEle IN @@cols FILTER collectionEle._belongsTo == orphanExecPlanID && collectionEle._created < @purgeLimitTimestamp REMOVE collectionEle IN @@cols',
+            {
+                'arr': orphanExecPlanIDsOlderThanThresholdDaysArray,
+                '@cols': collections[i],
+                'purgeLimitTimestamp': timestamp
+            }
+        )
+        let endCount = db._query('RETURN COUNT(@@cols)', {'@cols': collections[i]}).toArray()[0]
+        console.log(startCount - endCount, collections[i], 'objects deleted...')
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    let startCount = 0
+    let endCount = 0
+
+    console.log('### Working on executionPlan collection')
+    startCount = db._query('RETURN COUNT(@@cols)', {'@cols': 'executionPlan'}).toArray()[0]
+    db._query(aql`
+        FOR doc IN executionPlan
+            FILTER doc._id IN ${orphanExecPlanIDsOlderThanThresholdDaysArray}
+            REMOVE doc IN executionPlan
+    `)
+    endCount = db._query('RETURN COUNT(@@cols)', {'@cols': 'executionPlan'}).toArray()[0]
+    console.log(startCount - endCount, 'executionPlan objects deleted...')
+
+    // Deleted other collections and then the orphanExecPlanIDs
+
     const t2 = Date.now()
-    console.log({refDataSourceKeys: [refDataSourceKeys.length, `${t2 - t1} ms`]})
+    console.log(`All collections but dataSource purge in ${t2 - t1} ms`)
 
-    // remove orphan sources
+    ////////////////////////////////////////////////////////////////////////
 
-    const dss = db._query(aql`
-        FOR dsKey IN ${refDataSourceKeys}
-            LET ds = DOCUMENT('dataSource', dsKey)
-            LET isDsReferred =
-                0 < LENGTH(FOR e IN affects   FILTER e._to == ds._id LIMIT 1 RETURN 1) OR
-                0 < LENGTH(FOR e IN depends   FILTER e._to == ds._id LIMIT 1 RETURN 1) OR
-                0 < LENGTH(FOR e IN readsFrom FILTER e._to == ds._id LIMIT 1 RETURN 1) OR
-                0 < LENGTH(FOR e IN writesTo  FILTER e._to == ds._id LIMIT 1 RETURN 1)
-            FILTER NOT isDsReferred
-            REMOVE ds IN dataSource
-            RETURN ds._id // <--- delete this statement
-    `).toArray() // todo: delete this and ^
+
+    // STAGE 3: No `affects` and `depends` corresponding to dataSource (Need to scan all dataSources, and both edges foreach) -> delete dataSource
+    const dataSourceKeysInAffectUseArray = db._query(`
+        FOR source IN dataSource
+            FOR affectEdge in affects
+                FILTER affectEdge._to == source._id
+                COLLECT keys = source._key
+                RETURN keys
+    `).toArray()
+
+    const dataSourceKeysInAffectsDependsUseArray = dataSourceKeysInAffectUseArray.concat(
+        db._query(`
+            FOR source IN dataSource
+                FOR dependEdge in depends
+                    FILTER dependEdge._to == source._id
+                    COLLECT keys = source._key
+                    RETURN keys
+        `).toArray()
+    )
+
+    const dataSourceKeysInTotalUseArray = Array.from(new Set(dataSourceKeysInAffectsDependsUseArray))
+    const orphanDataSourceKeysOlderThanThresholdDaysArray = db._query(aql`
+        FOR source IN dataSource
+            FILTER source._key NOT IN ${dataSourceKeysInTotalUseArray} && source._created < ${timestamp}
+            RETURN source._key
+    `).toArray()
+
+    console.log('### Working on dataSource collection')
+    startCount = db._query('RETURN COUNT(@@cols)', {'@cols': 'dataSource'}).toArray()[0]
+    db._query(aql`
+        FOR source IN dataSource
+            FILTER source._key IN ${orphanDataSourceKeysOlderThanThresholdDaysArray}
+            REMOVE source in dataSource
+    `)
+    endCount = db._query('RETURN COUNT(@@cols)', {'@cols': 'dataSource'}).toArray()[0]
 
     const t3 = Date.now()
-    console.log({dss: [dss.length, `${t3 - t2} ms`]})
+    console.log(startCount - endCount, `dataSource objects deleted... in ${t3 - t2} ms`)
+    console.log('-------Purge completed-------')
 }
