de035956b3c2e18c0ef2465c57d497797463c29d
Oleksandr Vayda <oleksandr.vayda@gmail.com>
2024-07-19 16:34:06 +0200

    spline #1155 Move some consumer related entities to a foxx-api module and generate a TS model

---
+++ arangodb-foxx-api/src/main/scala/za/co/absa/spline/consumer/service/model/Frame.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2024 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.spline.consumer.service.model
+
+import java.{util => ju}
+
+/**
+ * Represents a frame of items with pagination information.
+ *
+ * @param items      items
+ * @param totalCount total count of items
+ * @param offset     offset
+ * @tparam T type of items
+ */
+case class Frame[T](
+  items: ju.List[T],
+  totalCount: Long,
+  offset: Int
+)
